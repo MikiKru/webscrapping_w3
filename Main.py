30f531b35b564510ef4f8a0bfd66e48e2d6c0b5c
MikiKru <michal_kruczkowski@o2.pl>
2020-04-18 16:10:23 +0200

    ws v7

--- Main.py
+++ Main.py
@@ -8,8 +8,12 @@ import pymysql, requests
 # 1. Wykonaj żądanie GET dla zadresu https://www.imdb.com/chart/top?ref_=nv_mv_250
 from bs4 import BeautifulSoup
 
+from Model import top250
+
 
 class ImdbScrapper:
+    def __init__(self):
+        self.movies = []
     # self.obiekt -> zakres wydoczności obejmuje całąklasę ImdbScrapper
     def getTop250(self):
         try:
@@ -32,25 +36,21 @@ class ImdbScrapper:
             years[index] = str(years[index]).split("(")[1].split(")")[0]
             ratings[index] = str(ratings[index]).split(">")[2].replace("</strong","")
             refs[index] = "https://www.imdb.com" + str(refs[index]).split('href="')[1].split('"')[0]
-            print(titles[index])
-            print(years[index])
-            print(ratings[index])
-            print(refs[index])
-            # self.getMovieDetails(refs[index])
+            director, stars = self.getMovieDetails(refs[index])
+            # zapis danych o filmie do obiektu modelu
+            movie = top250(titles[index], years[index], director, stars, ratings[index], refs[index])
+            print(movie)
     def getMovieDetails(self, url):
         details = requests.get(url)
         details_html = BeautifulSoup(details.content, 'html.parser')
-        print(details_html.prettify())
         # pobierz reżysera
         # pobierz gwiazdy 3 pozycje
-        kolumna = details_html.findAll(class_="credit_summary_item")
-        rezyser = (str(kolumna).split(">")[4])[:-3]
-        aktorzy = (str(kolumna).split("Stars:")[1].split(">")[2]).replace("</a", ""), \
-                  (str(kolumna).split("Stars:")[1].split(">")[4]).replace("</a", ""), \
-                  (str(kolumna).split("Stars:")[1].split(">")[6]).replace("</a", "")
-        print(aktorzy)
-        print(rezyser)
-
+        column = details_html.findAll(class_="credit_summary_item")
+        director = (str(column).split(">")[4])[:-3]
+        stars = (str(column).split("Stars:")[1].split(">")[2]).replace("</a", ""), \
+                  (str(column).split("Stars:")[1].split(">")[4]).replace("</a", ""), \
+                  (str(column).split("Stars:")[1].split(">")[6]).replace("</a", "")
+        return director, stars
 
 imdb = ImdbScrapper()
 imdb.getTop250()
--- Model.py
+++ Model.py
@@ -9,5 +9,5 @@ class top250:
         self.rating = rating
         self.link = link
     def __str__(self):
-        return '|%20s | %10s | %20s | %30s | %5s | %30s |' % \
+        return '|%100s | %5s | %50s | %100s | %5s | %50s |' % \
                (self.title, self.year, self.director, self.stars, self.rating, self.link)
